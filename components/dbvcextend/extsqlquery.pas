9b7e5b9e3a9a6a624e29b080406dff281e7ce53d
Alexander Shelemetyev <ex.lxndr@gmail.com>
2013-10-08 09:16:21 +0700

    add license

--- components/dbvcextend/extsqlquery.pas
+++ components/dbvcextend/extsqlquery.pas
@@ -1,5 +1,28 @@
 unit ExtSQLQuery;
 
+{
+MaCom - Free/Libre Management Company Information System
+TSQLQuery extension
+
+Copyright (C) 2013 Alexandr Shelemetyev
+https://github.com/egno/macom
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+
+Текст лицензии на русском: http://rusgpl.ru/
+}
+
 {$mode objfpc}{$H+}
 
 interface
